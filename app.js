fa2f4eef30df1786aabae098935935915be60aaa
kaya <kayaprod@cousera-something.com>
2016-11-05 17:09:06 -0400

    Files

--- app.js
+++ app.js
@@ -7,39 +7,56 @@ angular.module('ShoppingListCheckOff',[])
 
 // injection of ShoppingListCheckOffService
 ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+//controller
 function ToBuyShoppingController(ShoppingListCheckOffService){
   var ToBuyController = this;
     // A faire
     // retrieve items
+   // alert("DEBUT CONTROLEUR 1");
     ToBuyController.items = ShoppingListCheckOffService.getItems();
     //ToBuyController.lenght = ShoppingListCheckOffService.mylenght;
     //console.log("items to buy  now " +   ShoppingListCheckOffService.getItems());
    // console.log("items to buy  name : " +   ToBuyController.items[2].name + " items to buy quantity :" + ToBuyController.items[2].quantity);
     //console.log("items to buy ");
-    console.log("items lenght " +   ShoppingListCheckOffService.mylenght());
-    //ToBuyController.sayhello = ShoppingListCheckOffService.sayhello();
+    //console.log("items lenght " +   ShoppingListCheckOffService.mylenght()); //OK    
     //remove item from list to buy
     ToBuyController.removeItem = function(itemIndex){
       var item = {name:ToBuyController.items[itemIndex].name, quantity:ToBuyController.items[itemIndex].quantity };
       console.log("item en cours.. name :" + ToBuyController.items[itemIndex].name + " quantity : " + ToBuyController.items[itemIndex].quantity); // Ajout test
         ShoppingListCheckOffService.removeItem(itemIndex,item);
+        //alert("INVOCATION DU SERVICE DANS CONTROLEUR 1");
+        // invoke service bought_items
+        //ShoppingListCheckOffService.getListBought();        
     };
+    //alert("FIN CONTROLEUR 1");
 }
 
 AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+//controller
 function AlreadyBoughtShoppingController(ShoppingListCheckOffService){
   var AlreadyBoughtController = this;
-    // A faire
-    AlreadyBoughtController.items = ShoppingListCheckOffService.getListBought();
-  //  console.log("items bought  name : " +   AlreadyBoughtController.items[0].name + " items bought quantity :" + AlreadyBoughtController.items[0].quantity);
-  //    console.log(" length of AlreadyBoughtController : " + AlreadyBoughtController.items.length);
+    // test
+    //alert("DEBUT CONTROLEUR 2");    
+    AlreadyBoughtController.start = function(){
+      AlreadyBoughtController.items = ShoppingListCheckOffService.getListBought();
+         //console.log("INVOCATION DU CONTROLEUR NUM. 2");
+         alert("INVOCATION DU SERVICE DANS CONTROLEUR 2");         
+        var i;
+        for(i=0; i<AlreadyBoughtController.items.length;i++)
+        {
+          console.log("Objet vendu --> name :" + AlreadyBoughtController.items[i].name + " quantity :" + AlreadyBoughtController.items[i].quantity);
+        } 
+    };
+
+    //AlreadyBoughtController.items = ShoppingListCheckOffService.getListBought();  
 }
  // service
 function ShoppingListCheckOffService(){
   var service = this;
+  var count_Iteration = 0;
+
   // List of shopping items to buy (local list)
-  var items = [{name: "chocolate", quantity: 1},{name: "ham", quantity: 2},{name: "cookies", quantity: 10},{name: "cheese", quantity: 1},{name: "patatoes", quantity: 10}];
-  //var items = [0,1,2,3,4];
+  var items = [{name: "chocolate", quantity: 1},{name: "ham", quantity: 2},{name: "cookies", quantity: 10},{name: "cheese", quantity: 1},{name: "patatoes", quantity: 10}];  
   //List of items to buy
    var tobuy_items = [];
   // list of bought items
@@ -49,8 +66,7 @@ function ShoppingListCheckOffService(){
   service.getItems = function(){
     var i ;
     for(i=0; i< items.length;i++)
-    {
-      //console.log("items lenght " + items.lenght)
+    {      
       tobuy_items.push(items[i]);
     }
     return tobuy_items;
@@ -60,23 +76,23 @@ function ShoppingListCheckOffService(){
     return tobuy_items.length;
   };
 
-  /*service.sayhello = function(){
-    console.log("hello from service !");
-  };*/
    // Remove item of list to buy
-  service.removeItem = function (itemIdex,item){
-    //var tmp = [];
-    //tmp.push(tobuy_items[itemIdex]);
-    //add to bought list
-    //bought_items.push(tobuy_items[itemIdex]); ///test
+  service.removeItem = function (itemIdex,item){    
     bought_items.push(item);
-    //console.log("length bought_items "+ bought_items.length);
-   // console.log("name :" + bought_items[itemIdex].name + " quantity : " + bought_items[itemIdex].quantity);
+    console.log("length of bought_items after iteration "+ bought_items.length);  //COMMENTE 
+    console.log("value of item --> name : " + bought_items[count_Iteration].name + " quantity :" + bought_items[count_Iteration++].quantity);
     tobuy_items.splice(itemIdex, 1);
   };
 
-  service.getListBought = function(){
-    return bought_items;
+   service.getListBought = function(){
+    if(bought_items.length != 0)
+    {
+         alert("service getListBought");
+         console.log("service getListBought");
+        return bought_items;
+       
+    }
+       
   };
 }
 })();
